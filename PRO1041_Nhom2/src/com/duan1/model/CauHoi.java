38e786d0d0ae2025b17844de72fbb7e383a9b496
lequan155 <lequan155@gmail.com>
2019-12-03 16:05:15 +0700

    update
    update

---
+++ PRO1041_Nhom2/src/com/duan1/model/CauHoi.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.duan1.model;
+
+/**
+ *
+ * @author QUAN
+ */
+public class CauHoi {
+    int id;
+    String mon;
+    String deTH;
+    String deBai;
+    String dapAn;
+    String dapAnS1;
+    String dapAnS2;
+    String dapAnS3;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMon() {
+        return mon;
+    }
+
+    public void setMon(String mon) {
+        this.mon = mon;
+    }
+
+    public String getDeTH() {
+        return deTH;
+    }
+
+    public void setDeTH(String deTH) {
+        this.deTH = deTH;
+    }
+
+    public String getDeBai() {
+        return deBai;
+    }
+
+    public void setDeBai(String deBai) {
+        this.deBai = deBai;
+    }
+
+    public String getDapAn() {
+        return dapAn;
+    }
+
+    public void setDapAn(String dapAn) {
+        this.dapAn = dapAn;
+    }
+
+    public String getDapAnS1() {
+        return dapAnS1;
+    }
+
+    public void setDapAnS1(String dapAnS1) {
+        this.dapAnS1 = dapAnS1;
+    }
+
+    public String getDapAnS2() {
+        return dapAnS2;
+    }
+
+    public void setDapAnS2(String dapAnS2) {
+        this.dapAnS2 = dapAnS2;
+    }
+
+    public String getDapAnS3() {
+        return dapAnS3;
+    }
+
+    public void setDapAnS3(String dapAnS3) {
+        this.dapAnS3 = dapAnS3;
+    }
+    
+}
---
+++ PRO1041_Nhom2/src/com/duan1/model/HocSinh.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.duan1.model;
+
+/**
+ *
+ * @author QUAN
+ */
+public class HocSinh {
+    String ten;
+    String email;
+    String id;
+    String pass;
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+}
