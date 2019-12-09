e5b0fea21b164095e154bf8762c214575b51eb19
lequan155 <51352592+lequan155@users.noreply.github.com>
2019-12-09 10:48:50 +0700

    Update DangKyJFrame.java

--- PRO1041_Nhom2/src/com/duan1/ui/DangKyJFrame.java
+++ PRO1041_Nhom2/src/com/duan1/ui/DangKyJFrame.java
@@ -220,7 +220,7 @@ public class DangKyJFrame extends javax.swing.JFrame {
            DialogHelper.alert(this, "Tên học sinh chỉ được dùng ký tự chữ và khoảng trắng");
            return;
        }
-       else if (!txEmail.getText().matches("\\w+@+\\w+(\\.+\\w){1,2}")) {
+       else if (!txEmail.getText().matches("\\w+@\\w+(\\.\\w+){1,2}")) {
             DialogHelper.alert(this, "Nhập sai định dạng email");
             return;
         }
